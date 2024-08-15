4526dd7a2594a99f201747937478e0cb23e6b1a4
Daryl R Cox <dcox@justmacapps.net>
2024-08-15 15:17:22 -0500

    Initial Commit

---
+++ JustASegueAndUnwind1/JustASegueAndUnwind1App.swift
@@ -0,0 +1,17 @@
+//
+//  JustASegueAndUnwind1App.swift
+//  JustASegueAndUnwind1
+//
+//  Created by Daryl Cox on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct JustASegueAndUnwind1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
